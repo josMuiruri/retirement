42b4ec7167565e99832ee291b8d87c879e379353
josmuiruri <josemuiruri51@gmail.com>
2024-05-15 17:04:30 +0300

    devopment of profile app - forms, urls

---
+++ retirement/my_profile/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . views import view_profile, edit_profile
+
+urlpatterns = [
+    path('profile/', view_profile, name='view_profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
+]
